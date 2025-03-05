af5b27ddb7ba36163df61e56e2eb2c9992e05b6a
Hardik Tripathi <134262806+hardik-tripathi@users.noreply.github.com>
2025-03-05 15:54:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	function firstNonRepeatedChar(str) {
+  const charCount = new Map();
+
+  for (let char of str) {
+    charCount.set(char, (charCount.get(char) || 0) + 1);
+  }
+
+  for (let char of str) {
+    if (charCount.get(char) === 1) {
+      return char;
+    }
+  }
+
+  return null;
+}
+
+const input = prompt("Enter a string");
+alert(firstNonRepeatedChar(input));
+
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
